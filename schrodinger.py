adace2b68dea3a40e18a08a37e3ab55f760547c6
NorthSeaPark <33457633+NorthSeaPark@users.noreply.github.com>
2019-06-19 13:14:19 -0400

    Add a comment

--- schrodinger.py
+++ schrodinger.py
@@ -306,5 +306,5 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
 # requires either mencoder or ffmpeg to be installed on your system
 
 #anim.save('schrodinger_barrier.mp4', fps=15, extra_args=['-vcodec', 'libx264'])
-
+#This is just a "Pull Request" test
 pl.show()
